0fea25f350d07b5fd8c8973fba32efb65b530da9
drsavi <deb.rar.savi@gmail.com>
2021-08-03 13:05:36 -0300

    Alterações na projeto
    Adicionado switch case

--- Banco/src/banco/Conta.java
+++ Banco/src/banco/Conta.java
@@ -1,16 +1,48 @@
 package banco;
+import java.util.Scanner;
+
 
 public class Conta {
 	int numero;
 	String titular;
 	double saldo;
 	
-	void sacar(double valor) {
-		this.saldo -= valor;
+	boolean sacar(double valor) {
+		if(valor > this.saldo){
+			return false;
+		 }else 
+		 	this.saldo -= valor;
+			 return true;
 	}
 	
 	void depositar(double valor) {
 		this.saldo += valor;
+		System.out.println("\nDeposito realizado com sucesso!");
+		System.out.println("\nNovo saldo: " + this.saldo + "\n");
+	}
+
+
+	void opcaoConsultarInformacoes(){
+		System.out.println("\nNumero da conta: " + this.numero);
+		System.out.println("\nTitular: " + this.titular);
+		System.out.println("\nSaldo: " + this.saldo);
+	}
+
+	double opcaoSacar(Scanner sc){
+		System.out.println("\nQual valor deseja sacar?");
+		double valor = sc.nextDouble();
+		if(this.sacar(sc.nextDouble())){
+			System.out.println("\nSaque efetuado! Novo saldo e R$ " + this.saldo);
+		}else{
+			System.out.println("\nSaldo insuficiente!");
+			System.out.println("\nSeu saldo e de apenas R$ " + this.saldo);
+		}
+	}
+
+	double opcaoDepositar(){
+
+		System.out.println("\nQual valor deseja depositar?");
+		this.depositar(sc.nextDouble());
 	}
 
 }
--- Banco/src/banco/Main.java
+++ Banco/src/banco/Main.java
@@ -7,20 +7,34 @@ public class Main {
 		Conta minhaConta = new Conta();
 		System.out.println("Digite o numero da sua conta: ");
 		minhaConta.numero = sc.nextInt();
-		String lixo = sc.nextLine();
+		sc.nextLine();
 		System.out.println("Digite o nome do titular desta conta: ");
 		minhaConta.titular = sc.nextLine();
-		System.out.println("Conta da " + minhaConta.titular + ", numero: " + minhaConta.numero);
+		System.out.println("Titular: " + minhaConta.titular + ", numero da conta: " + minhaConta.numero);
 		System.out.println("Digite o saldo inicial desta conta: ");
 		minhaConta.saldo = sc.nextDouble();
-		System.out.println("Seu saldo atual e R$ " + minhaConta.saldo);
-		System.out.println("Digite quanto quer sacar: ");
+		System.out.println("Saldo atual: R$ " + minhaConta.saldo);
+		System.out.println("Digite quanto deseja sacar: ");
 		minhaConta.sacar(sc.nextDouble());
-		System.out.println("Seu saldo atual e R$ " + minhaConta.saldo);
-		System.out.println("Digite quanto quer depositar: ");
+		System.out.println("Seu atual: R$ " + minhaConta.saldo);
+		System.out.println("Digite quanto deseja depositar: ");
 		minhaConta.depositar(sc.nextDouble());
-		System.out.println("Seu saldo atual e R$ " + minhaConta.saldo);
-		
+		System.out.println("Saldo atual: R$ " + minhaConta.saldo);
+
+
+		int opcao = 0;
+		while(opcao != 4){
+			System.out.println("1 - Consultar Informações\n2 - Sacar\n3 - Depositar\n4 - Sair da Aplicacao");
+			opcao = sc.nextInt();
+			
+			double valor = sc.nextDouble();
+			switch(opcao){
+				case 1 :	minhaConta.opcaoConsultarInformacoes();	break;
+				case 2 :	minhaConta.saldo -= minhaConta.opcaoSacar(sc);		break; //colocar uma entrada aqui
+				case 3 :	minhaConta.opcaoDepositar();			break;
+				default:	System.out.println("Say goodbye!");		break;
+			}
+		}
 		sc.close();
 	}
 }
